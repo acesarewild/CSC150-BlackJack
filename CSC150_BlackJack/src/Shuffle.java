e1c2e2228e3511c05848303446f3e7bbfbb7ad1b
alexander quang <aquang@student.neumont.edu>
2013-02-27 10:57:34 -0700

    overloaded shuffle method to keep the img paths the same index as the cards

--- CSC150_BlackJack/src/Shuffle.java
+++ CSC150_BlackJack/src/Shuffle.java
@@ -16,6 +16,7 @@ public class Shuffle
 	{
 		generator = new Random();
 	}
+	
 	//-----------------------------------------------------
 	//  @param array
 	//  NOTE: This method alters this argument
@@ -34,4 +35,29 @@ public class Shuffle
 			array[j] = temp;
 		}	
 	}
+	
+	//-----------------------------------------------------
+		//  overloaded to shuffle the deck and img paths
+		//  @param array
+		//  NOTE: This method alters this argument
+		//-----------------------------------------------------
+		public void shuffleArray(Card[] array, String[] paths)
+		{
+			for (int i = 0; i < array.length; i++)
+			{
+				//Randomly choose index j from the remainder of the array
+				int randomNumber = generator.nextInt(array.length-i); //Fischer Yates shuffle
+				int j = i + randomNumber;
+				
+				//Swap array[i] with array[j] (i being the current position in the array, j being the random number)
+				Card temp = array[i];
+				array[i] = array [j];
+				array[j] = temp;
+				
+				//Swap paths[i] with paths[j] (i being the current position in the array, j being the random number)
+				String temp2 = paths[i];
+				paths[i] = paths[j];
+				paths[j] = temp2;
+			}	
+		}
 }
